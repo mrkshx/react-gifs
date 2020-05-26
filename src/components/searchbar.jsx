d8f11893f8489e535ee0b4d3b55504da8eb3a7b9
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-27 00:18:49 +0200

    created searchbar.jsx with input

---
+++ src/components/searchbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Searchbar extends Component {
+  render() {
+    return (
+      <input type="text" className="form-search form-control"/>
+    );
+  }
+}
+
+export default Searchbar;
