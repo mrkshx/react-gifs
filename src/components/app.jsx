28f61c16ab3d475f4b413a705e6bde1738de08e6
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-27 00:26:25 +0200

    added searchbar to app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Searchbar from './searchbar.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="left-scene"></div>
+        <div className="left-scene">
+          <Searchbar />
+        </div>
         <div className="right-scene"></div>
       </div>
     );
