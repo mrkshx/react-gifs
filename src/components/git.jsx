b877d8fd628a478afca8249c22c1e2def49d3891
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-28 15:03:26 +0200

    rendering a fixed gif

--- src/components/git.jsx
+++ src/components/git.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
+    const src = 'https://media1.giphy.com/media/KGZCDBoaOPILHJc7RT/giphy.gif';
     return (
-
+      <img src={src} alt="" className="gif" />
     );
   }
 }
