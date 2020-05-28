4b8bacd947a6f4687f79ee9e55a64dab7a10d1f3
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-28 17:55:36 +0200

    log inout in console

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class Searchbar extends Component {
+  handleUpdate = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
-      <input type="text" className="form-search form-control"/>
+      <input
+        type="text"
+        className="form-search form-control"
+        onChange={this.handleUpdate}
+      />
     );
   }
 }
