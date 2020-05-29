a7ed47056325e69aabbc3844a5e8292857d3f5c1
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-29 16:55:52 +0200

    fix issue: selected gif from list will now be shown on the left

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -4,7 +4,7 @@ import Gif from './gif.jsx';
 
 class GifList extends Component {
   renderList = () => {
-    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />);
+    return this.props.gifs.map(gif => <Gif selectFunction={this.props.selectGifFunction} id={gif.id} key={gif.id} />);
   }
 
   render() {
