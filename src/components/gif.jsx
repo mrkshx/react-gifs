b5d910781de15c4869424fecc8aaddef39454235
Markus Mengel <markus.mengelhx@gmail.com>
2020-05-29 22:08:14 +0200

    destructures props in gif.jsx

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   handleClick = (event) => {
-    this.props.selectFunction(event.target.id);
+    const { selectFunction } = this.props;
+    selectFunction(event.target.id);
   }
 
   render() {
-    const src = `https://media1.giphy.com/media/${this.props.id}/giphy.gif`;
+    const { id } = this.props;
+    const src = `https://media1.giphy.com/media/${id}/giphy.gif`;
     return (
       <img
         src={src}
         alt=""
         className="gif"
         onClick={this.handleClick}
-        id={this.props.id}
+        id={id}
       />
     );
   }
